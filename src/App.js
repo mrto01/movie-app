a92fee3f430239c629401defaac4644078d2cb9a
Daidz <lodaivll01@gmail.com>
2023-05-21 17:49:52 +0700

    test ci/cd

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <a
-            href="https://youtube.com"
+            href="https://facebook.com"
         >
-            I think we done
-            Learn react version 3
+            Learn react , link to facebook
         </a>
       </header>
     </div>
